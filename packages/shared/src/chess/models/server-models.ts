0b09db6c6f27557171e0bdfc7107b46ac32232c3
AbdulsProjects <106890796+AbdulsProjects@users.noreply.github.com>
2024-06-23 18:25:03 +0100

    Converted the project into a workspace, seperating the project into client, server and shared folders and setting up dependancies

---
+++ packages/shared/src/chess/models/server-models.ts
@@ -0,0 +1,15 @@
+import { Square } from "./chess-models"
+
+export interface Lobby {
+    lobbyId: string,
+    lobbyName: string,
+    lobbyPassword: string | null,
+    gameType: string,
+    white: string | null,
+    black: string | null,
+    board: any | null
+    suggestedSquares: {
+        white: Square[],
+        black: Square[]
+    }
+}
