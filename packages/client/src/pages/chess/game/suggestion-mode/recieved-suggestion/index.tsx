79d48ddc57d98fbb4895e09f9d4ca05d35c7ebdd
AbdulsProjects <106890796+AbdulsProjects@users.noreply.github.com>
2024-07-03 18:37:55 +0100

    Started refactoring the code base to remove the createCallBack function

--- packages/client/src/pages/chess/game/suggestion-mode/recieved-suggestion/index.tsx
+++ packages/client/src/pages/chess/game/suggestion-mode/recieved-suggestion/index.tsx
@@ -15,13 +15,22 @@ export const RecievedSuggestion = (props: Props) => {
     const { onlineState, createCallback }  = useContext(WsContext) as IWsContext;
     const currentSuggestion = onlineState.lobby?.suggestedSquares[onlineState.colour === 'white' ? 'black' : 'white'];
 
-    //Creating the callback for accepting a suggestion
-    useEffect(() => {
-        createCallback('accept-suggestion', (response) => {
-            const { _squares, _outcome, _gameState } = response.lobby.board;
+    onlineState.wsConn?.addEventListener("message", (event) => {
+        const data = JSON.parse(event.data);
+        if (data.method === 'accept-suggestion') {
+            console.log("hit")
+            const { _squares, _outcome, _gameState } = data.lobby.board;
             props.SetBoard(new Board(_squares, _outcome, _gameState));
-        });
-    }, []);
+        };
+    });
+
+    //Creating the callback for accepting a suggestion
+    // useEffect(() => {
+    //     createCallback('accept-suggestion', (response) => {
+    //         const { _squares, _outcome, _gameState } = response.lobby.board;
+    //         props.SetBoard(new Board(_squares, _outcome, _gameState));
+    //     });
+    // }, []);
 
     const DeclineSuggestion = () => {
         
